7aea6c78b63aacffe91e79802be0d9c4743f5222
yaswanth <ya8wanth@gmail.com>
2023-03-24 09:55:35 +0530

    addChocolate function added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,37 @@
-﻿namespace MyProject;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
 class Program
 {
-    static void Main(string[] args)
+    public static Dictionary<string, int> chocolates = new Dictionary<string, int> {
+        {"green", 0},
+        {"blue", 0},
+        {"red", 0},
+        {"pink", 0}
+    };
+    public static Stack<String> dispenserStack = new Stack<String>();   //to maintain pushing order
+    public void addChocolates(string color, int count)
     {
-        Console.WriteLine("Hello, World!");
+        chocolates[color] += count;
+        for(int i = 0; i < count; i++)
+        {
+            dispenserStack.Push(color);
+        }
+       
+    }
+    
+    public static void display()
+    {
+        while (dispenserStack.Count > 0)
+            Console.Write(dispenserStack.Pop() + ",");
+    }
+    public static void Main(string[] args)
+    {
+        Program dispenser = new Program();
+
+        dispenser.addChocolates("green", 2);
+        dispenser.addChocolates("red", 2);
+
+        display();
     }
 }
