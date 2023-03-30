8fbac951df8f7638364934c651a6a24f273a379f
yaswanth <ya8wanth@gmail.com>
2023-03-30 15:52:55 +0530

    Final code

--- Program.cs
+++ Program.cs
@@ -30,21 +30,39 @@ class Program
     public void removeChocolates(int count)
     {
         Console.WriteLine("remove items from top---------");
+        string removedChocolate;
         for (int i = 0; i < count; i++)
         {
-            Console.WriteLine(dispenserStack.Pop());   
+           removedChocolate = dispenserStack.Pop();
+            chocolates[removedChocolate]--;
         }
         Console.WriteLine("-------------------------------");
         Console.WriteLine("");
     }
 
-    static void dispenseChocolatesOfColor(string color)
+    static void dispenseChocolatesOfColor(string color,int num)
     {
-        int colorCOunt = chocolates[color];
-        for(int i=0;i<colorCOunt; i++)
+        //int colorCOunt = chocolates[color];
+        chocolates[color] -= num;
+        Stack<String> duplicateStack = new Stack<String>();
+      
+        foreach(string i in dispenserStack)
         {
-            Console.WriteLine(color);
+            duplicateStack.Push(i);
         }
+        dispenserStack.Clear(); 
+        foreach(string i in duplicateStack)
+        {
+            if(i==color && num>0)
+            {
+                num--;
+                continue;
+            }
+            
+            dispenserStack.Push(i);
+            
+        }
+
     }
 
     public static void display()
@@ -60,17 +78,34 @@ class Program
 
     public static void dispenseChocolates(int num)
     {
-        Console.WriteLine("chocolates at bottom based on count -----");
+        //Console.WriteLine("chocolates at bottom based on count -----");
         int start = chocolateCount - num;
         int ctne = 0;
+        Stack<String> duplicateStack = new Stack<String>();
         foreach (string i in dispenserStack)
         {
+           
             ctne++;
             if (ctne > start)
             {
-                Console.WriteLine(i);
+                //Console.WriteLine(i);
+                chocolates[i]--;
+            }
+            else
+            {
+                duplicateStack.Push(i);
             }
         }
+        
+       // Console.WriteLine("step 2 completed");
+        dispenserStack.Clear();
+        foreach (string i in duplicateStack)
+        {
+            dispenserStack.Push(i);
+            //Console.WriteLine(i);
+        }
+        
+        duplicateStack.Clear();
         Console.WriteLine("------------------------------------");
         Console.WriteLine("");
     }
@@ -88,7 +123,7 @@ class Program
 
     public void sortChocolateBasedOnCount()
     {
-        var sortedDict = from entry in chocolates orderby entry.Value ascending select entry;
+        var sortedDict = from entry in chocolates orderby entry.Value descending select entry;
 
         foreach (var item in sortedDict)
         {
@@ -104,12 +139,45 @@ class Program
             chocolates.Remove(oldColor);  // remove the original key-value pair
             chocolates.Add(newColor, value);  // add a new key-value pair with the new key name and the same value
         }
+        Stack<string> newStack = new Stack<string>();
+        foreach (string color in dispenserStack)
+        {
+            if (color == oldColor)
+            {
+                newStack.Push(newColor);
+            }
+            else
+            {
+                newStack.Push(color);
+            }
+        }
+        dispenserStack.Clear();
+        foreach(string color in newStack)
+        {
+            dispenserStack.Push(color);
+        }
+        newStack.Clear();
     }
 
     public void removeChocolateOfColor(string chocolate)
     {
         if (chocolates[chocolate]>1)
         chocolates[chocolate]--;
+        string outChocolate;
+        Stack<string> newStack = new Stack<string>();
+        while(dispenserStack.Count > 0)
+        {
+            outChocolate = dispenserStack.Pop();
+            if ( outChocolate== chocolate)
+            {
+                break;
+            }
+            newStack.Push(outChocolate);
+        }
+        while(newStack.Count > 0)
+        {
+            dispenserStack.Push(newStack.Pop());
+        }
     }
 
     public int dispenseRainbowChocolates()
@@ -119,7 +187,6 @@ class Program
         {
             if(pair.Value >= 3)
             {
-                Console.WriteLine(pair.Value);
                 count = count + (pair.Value) / 3;
             }
             
@@ -132,14 +199,21 @@ class Program
 
         dispenser.addChocolates("green", 3);
         dispenser.addChocolates("red", 2);
+        dispenser.addChocolates("green", 1);
+        dispenser.addChocolates("blue", 2);
+        display();
         //dispenser.noOfChocolates();
         //dispenseChocolates(3);
-        //dispenseChocolatesOfColor("green");
-        //removeChocolates(2);
+       // dispenser.noOfChocolates();
+        //dispenseChocolatesOfColor("green",2);
+
+        //dispenser.noOfChocolates();
+        //dispenser.removeChocolates(2);
         //dispenser.changeChocolateColorAllOfxCount("green", "white");
         //dispenser.removeChocolateOfColor("green");
-        dispenser.sortChocolateBasedOnCount();
+        //dispenser.sortChocolateBasedOnCount();
         Console.WriteLine(dispenser.dispenseRainbowChocolates());
-       
+        display();
+
     }
 }
